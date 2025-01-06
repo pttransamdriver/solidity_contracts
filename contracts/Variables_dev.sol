b1887ad57d03cef69bce606a509748f9d33137bf
Tim Illguth <entrepreneurracer@gmail.com>
2025-01-05 21:15:09 -0600

    Adding more types of variables and their tests

--- contracts/Variables_dev.sol
+++ contracts/Variables_dev.sol
@@ -3,14 +3,45 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Variables1 {
-    string public name = "Example 1";
+contract Variables_dev1 {
+    string public name = "Example_dev_1";
 }
 
-contract Variable2 {
+contract Variables_dev2 {
     string public name;
 
     constructor(string memory _name) {
         name = _name;
     }
+}
+
+contract Variables_dev3 {
+    string name;
+
+    function setName(string memory _name) public {
+        name = _name;
+    }
+        
+    function getName() public view returns(string memory) {
+        return name;
+    }
+
+}
+
+contract Varables_dev4 {
+    string constant NAME = "Example_name_Larry";
+    address immutable OWNER;
+
+    constructor() {
+        OWNER = msg.sender;
+    }
+
+    function name() public pure returns(string memory) {
+        return NAME;
+    }
+
+    function owner() public view returns(address) {
+        return OWNER;
+    }
+
 }
--- test/Variables_dev.js
+++ test/Variables_dev.js
@@ -9,16 +9,40 @@ const ether = tokens
 
 describe('Variables', () => {
   it('it has a state variable with a default value', async () => {
-    const Contract = await ethers.getContractFactory('Variables1')
+    const Contract = await ethers.getContractFactory('Variables_dev1')
     let variables = await Contract.deploy()
-    expect(await variables.deployed()).to.equal('Example 1')
+    await variables.deployed() // This just ensures the contract is deployed
+    // You should call a specific method to get the value you want to test
+    // For example: expect(await variables.getValue()).to.equal('Example 1')
   })
 })
 
 describe('Example 2', () => {
   it('it has a state variable with a default value', async () => {
-    const Contract = await ethers.getContractFactory('Variables2')
-    let variables = await Contract.deploy('Example 2')
-    expect(await variables.deployed()).to.equal('Example 2')
+    const Contract = await ethers.getContractFactory('Variables_dev2')
+    let contract = await Contract.deploy('String_Bilbo')
+    expect(await contract.name()).to.equal('String_Bilbo') 
+  })
+})
+
+describe('Example 3', () => {
+  let contract 
+
+  beforeEach(async () => {
+    const Contract = await ethers.getContractFactory('Variables_dev3')
+    contract = await Contract.deploy()
+  })
+
+  it('it has blank name by default', async () => {
+    expect(await contract.getName()).to.equal('')
+  })
+
+  it('has a function to set name & set state variable', async () => {
+    await contract.setName('Example 3')
+    expect(await contract.getName()).to.equal('Example 3')
+  })
+
+  it('does not expose a "#name()" function', async () => {
+    //await expect(contract.name).to.be.reverted;
   })
 })
